14b665131d2a2f86d19505ef17840b1fa3c59860
hainet <20400616+hainet@users.noreply.github.com>
2018-12-06 11:24:37 +0900

    失敗したジョブをクリーニングする手法を検討する。

---
+++ failure-cleaner/src/test/java/com/hainet/failure/cleaner/FailureCleanerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hainet.failure.cleaner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FailureCleanerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
