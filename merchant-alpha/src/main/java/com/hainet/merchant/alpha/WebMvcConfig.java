fa892a89bd1cd620dc777a4fbe0422146f9236ad
hainet <20400616+hainet@users.noreply.github.com>
2018-12-06 11:24:52 +0900

    merchant-alpha

---
+++ merchant-alpha/src/main/java/com/hainet/merchant/alpha/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.hainet.merchant.alpha;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class WebMvcConfig {
+
+    @Bean
+    public RestTemplate restTemplate(final RestTemplateBuilder builder) {
+        return builder
+                .rootUri("http://localhost:8080")
+                .build();
+    }
+}
---
+++ merchant-alpha/src/test/java/com/hainet/merchant/alpha/MerchantAlphaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hainet.merchant.alpha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MerchantAlphaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
